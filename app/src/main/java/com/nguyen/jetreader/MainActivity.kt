f2a2b916b16f128f584998179ff6248891003258
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-32G-1T.local>
2025-10-04 16:41:13 -0700

    b3: saving a user to Firestore database - test user

--- app/src/main/java/com/nguyen/jetreader/MainActivity.kt
+++ app/src/main/java/com/nguyen/jetreader/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.nguyen.jetreader
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.firebase.firestore.FirebaseFirestore
 import com.nguyen.jetreader.ui.theme.JetReaderTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +21,18 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JetReaderTheme {
+                val user = mutableMapOf<String, Any>()
+                user["first"] = "Ada"
+                user["last"] = "Lovelace"
+                val database = FirebaseFirestore.getInstance()
+
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    database.collection("users").add(user).addOnSuccessListener {
+                        Log.d("TAGG", "onCreate: ${it.id}")
+                    }.addOnFailureListener {
+                        Log.d("TAGG", "onCreate: ${it.message}")
+                    }
+
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)

46baf3332638ca4c4641610db4b3bf70db660171
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-32G-1T.local>
2025-10-04 16:46:43 -0700

    b4: setting up dependency injection with Hilt

--- app/src/main/java/com/nguyen/jetreader/MainActivity.kt
+++ app/src/main/java/com/nguyen/jetreader/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.nguyen.jetreader
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -12,27 +11,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.google.firebase.firestore.FirebaseFirestore
 import com.nguyen.jetreader.ui.theme.JetReaderTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             JetReaderTheme {
-                val user = mutableMapOf<String, Any>()
-                user["first"] = "Ada"
-                user["last"] = "Lovelace"
-                val database = FirebaseFirestore.getInstance()
-
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    database.collection("users").add(user).addOnSuccessListener {
-                        Log.d("TAGG", "onCreate: ${it.id}")
-                    }.addOnFailureListener {
-                        Log.d("TAGG", "onCreate: ${it.message}")
-                    }
-
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
---
+++ app/src/main/java/com/nguyen/jetreader/ReaderApplication.kt
@@ -0,0 +1,8 @@
+package com.nguyen.jetreader
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ReaderApplication : Application() {
+}

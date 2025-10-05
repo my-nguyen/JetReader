db42ae6013b936ff7e2be5facc1bb05d067e83b3
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-32G-1T.local>
2025-10-04 18:56:47 -0700

    b10: animating the SplashScreen

--- app/src/main/java/com/nguyen/jetreader/screens/SplashScreen.kt
+++ app/src/main/java/com/nguyen/jetreader/screens/SplashScreen.kt
@@ -1,5 +1,7 @@
 package com.nguyen.jetreader.screens
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -12,21 +14,42 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.animation.OvershootInterpolator
 import androidx.navigation.NavHostController
+import com.nguyen.jetreader.navigation.ReaderScreens
+import kotlinx.coroutines.delay
 
 @Preview
 @Composable
 fun SplashScreen(navController: NavHostController = NavHostController(LocalContext.current)) {
+    val scaleState = remember { Animatable(0f) }
+
+    LaunchedEffect(key1 = true) {
+        scaleState.animateTo(
+            targetValue = 0.9f,
+            animationSpec = tween(
+                durationMillis = 800,
+                easing = { OvershootInterpolator(8f).getInterpolation(it) }
+            )
+        )
+        delay(2000L)
+        navController.navigate(ReaderScreens.LoginScreen.name)
+    }
+
     Surface(
         modifier = Modifier
             .padding(15.dp)
-            .size(330.dp),
+            .size(330.dp)
+            .scale(scaleState.value),
         shape = CircleShape,
         color = Color.White,
         border = BorderStroke(width = 2.dp, Color.LightGray),

4702a9d2c81665d46f2d7078502b799a5912d541
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-32G-1T.local>
2025-10-05 08:53:40 -0700

    b16: finishing login and create account logic

--- app/src/main/java/com/nguyen/jetreader/screens/login/LoginScreen.kt
+++ app/src/main/java/com/nguyen/jetreader/screens/login/LoginScreen.kt
@@ -2,8 +2,11 @@ package com.nguyen.jetreader.screens.login
 
 import android.util.Log
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -27,24 +30,52 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.nguyen.jetreader.R
 import com.nguyen.jetreader.components.EmailInput
 import com.nguyen.jetreader.components.Logo
 import com.nguyen.jetreader.components.PasswordInput
 
 @Composable
 fun LoginScreen(navController: NavHostController) {
+    val showLoginForm = rememberSaveable { mutableStateOf(true) }
+
     Surface(modifier = Modifier.fillMaxSize()) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Top
         ) {
             Logo()
-            UserForm(loading = false, isCreateAccount = false) { email, password ->
-                Log.d("TAGG", "LoginScreen, Email: $email, Password: $password")
+            if (showLoginForm.value) {
+                UserForm(loading = false, isCreateAccount = false) { email, password ->
+                    Log.d("TAGG", "LoginScreen, Email: $email, Password: $password")
+                }
+            } else {
+                UserForm(loading = false, isCreateAccount = true) { email, password ->
+                    Log.d("TAGG", "LoginScreen, Email: $email, Password: $password")
+                }
             }
+        }
+        Spacer(modifier = Modifier.height(15.dp))
+        Row(
+            modifier = Modifier.padding(15.dp),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text = "New User?")
+            val text = if (showLoginForm.value) "Sign up" else "Login"
+            Text(
+                text = text,
+                modifier = Modifier
+                    .clickable { showLoginForm.value = !showLoginForm.value }
+                    .padding(start = 5.dp),
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.secondary
+            )
         }
     }
 }
@@ -72,6 +103,14 @@ fun UserForm(
             .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        if (isCreateAccount) {
+            Text(
+                text = stringResource(R.string.create_acct),
+                modifier = Modifier.padding(4.dp)
+            )
+        } else {
+            Text(text = "")
+        }
         EmailInput(
             emailState = email,
             enabled = !loading,
@@ -94,6 +133,7 @@ fun UserForm(
             validInputs = valid
         ) {
             onDone(email.value.trim(), password.value.trim())
+            keyboardController?.hide()
         }
     }
 }

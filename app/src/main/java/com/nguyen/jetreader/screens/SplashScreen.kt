227d8399a0c0f2b195c6ffde0d1c034f017f82d0
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-32G-1T.local>
2025-10-04 18:44:44 -0700

    b9: creating the SplashScreen

--- app/src/main/java/com/nguyen/jetreader/screens/SplashScreen.kt
+++ app/src/main/java/com/nguyen/jetreader/screens/SplashScreen.kt
@@ -1,10 +1,52 @@
 package com.nguyen.jetreader.screens
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 
+@Preview
 @Composable
-fun SplashScreen(navController: NavHostController) {
-    Text(text = "Splash Screen")
+fun SplashScreen(navController: NavHostController = NavHostController(LocalContext.current)) {
+    Surface(
+        modifier = Modifier
+            .padding(15.dp)
+            .size(330.dp),
+        shape = CircleShape,
+        color = Color.White,
+        border = BorderStroke(width = 2.dp, Color.LightGray),
+    ) {
+        Column(
+            modifier = Modifier.padding(1.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                "Reader App",
+                style = MaterialTheme.typography.displayMedium,
+                color = Color.Red.copy(0.5f)
+            )
+            Spacer(modifier = Modifier.height(15.dp))
+            Text(
+                "\"Read. Change. Yourself\"",
+                style = MaterialTheme.typography.headlineSmall,
+                color = Color.Gray
+            )
+        }
+    }
 }

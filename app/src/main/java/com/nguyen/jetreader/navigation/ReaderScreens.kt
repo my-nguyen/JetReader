1e6deda2179bcc8bd8927f34600e77816e02c355
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-32G-1T.local>
2025-10-04 17:05:42 -0700

    b6: setting up the ReaderScreen enum & app routing

--- app/src/main/java/com/nguyen/jetreader/navigation/ReaderScreens.kt
+++ app/src/main/java/com/nguyen/jetreader/navigation/ReaderScreens.kt
@@ -1,4 +1,28 @@
 package com.nguyen.jetreader.navigation
 
 enum class ReaderScreens {
+    SplashScreen,
+    LoginScreen,
+    AccountScreen,
+    HomeScreen,
+    SearchScreen,
+    DetailScreen,
+    UpdateScreen,
+    StatsScreen;
+
+    companion object {
+        fun fromRoute(route: String?): ReaderScreens =
+            when (route?.substringBefore("/")) {
+                SplashScreen.name -> SplashScreen
+                LoginScreen.name -> LoginScreen
+                AccountScreen.name -> AccountScreen
+                HomeScreen.name -> HomeScreen
+                SearchScreen.name -> SearchScreen
+                DetailScreen.name -> DetailScreen
+                UpdateScreen.name -> UpdateScreen
+                StatsScreen.name -> StatsScreen
+                null -> SplashScreen
+                else -> throw java.lang.IllegalArgumentException("Route $route is not recognized")
+            }
+    }
 }

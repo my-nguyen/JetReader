b86073a9aa27f10b713f0b9d977cd2b0d03f1d01
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-32G-1T.local>
2025-10-05 14:34:17 -0700

    b26: finishing up the top section - reading and user profile

--- app/src/main/java/com/nguyen/jetreader/screens/home/HomeScreen.kt
+++ app/src/main/java/com/nguyen/jetreader/screens/home/HomeScreen.kt
@@ -1,21 +1,27 @@
 package com.nguyen.jetreader.screens.home
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.Logout
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -32,6 +38,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -58,9 +65,35 @@ fun HomeScreen(navController: NavController = NavController(LocalContext.current
 
 @Composable
 fun HomeContent(navController: NavController) {
+    val email = Firebase.auth.currentUser?.email
+    val currentUserName =
+        if (!email.isNullOrEmpty()) Firebase.auth.currentUser?.email?.split('@')?.get(0)
+        else "N/A"
+
     Column(modifier = Modifier.padding(2.dp), verticalArrangement = Arrangement.SpaceEvenly) {
         Row(modifier = Modifier.align(alignment = Alignment.Start)) {
             TitleSection(title = "Your reading \n activity right now...")
+            Spacer(modifier = Modifier.fillMaxWidth(0.7f))
+            Column {
+                Icon(
+                    imageVector = Icons.Filled.AccountCircle,
+                    contentDescription = "Profile",
+                    modifier = Modifier
+                        .clickable { navController.navigate(ReaderScreens.StatsScreen.name) }
+                        .size(45.dp),
+                    tint = MaterialTheme.colorScheme.secondary
+                )
+                Text(
+                    text = currentUserName!!,
+                    modifier = Modifier.padding(2.dp),
+                    style = MaterialTheme.typography.titleSmall,
+                    color = Color.Red,
+                    fontSize = 15.sp,
+                    maxLines = 1,
+                    overflow = TextOverflow.Clip
+                )
+                HorizontalDivider()
+            }
         }
     }
 }
@@ -99,7 +132,7 @@ fun ReaderAppBar(title: String, showProfile: Boolean = true, navController: NavC
                 Icon(
                     imageVector = Icons.AutoMirrored.Filled.Logout,
                     contentDescription = "Logout",
-                    tint = Color.Green.copy(0.4f)
+                    // tint = Color.Green.copy(0.4f)
                 )
             }
         },
--- app/src/main/java/com/nguyen/jetreader/screens/stats/StatsScreen.kt
+++ app/src/main/java/com/nguyen/jetreader/screens/stats/StatsScreen.kt
@@ -1,9 +1,10 @@
 package com.nguyen.jetreader.screens.stats
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 
 @Composable
 fun StatsScreen(navController: NavHostController) {
-
+    Text("Stats Screen")
 }

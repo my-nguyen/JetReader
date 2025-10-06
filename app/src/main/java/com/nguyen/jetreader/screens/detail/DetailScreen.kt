463cba8e52fea141512021e0a6d1a4c56dcc2136
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-32G-1T.local>
2025-10-06 16:05:50 -0700

    b46: adding the book description

--- app/src/main/java/com/nguyen/jetreader/screens/detail/DetailScreen.kt
+++ app/src/main/java/com/nguyen/jetreader/screens/detail/DetailScreen.kt
@@ -1,5 +1,6 @@
 package com.nguyen.jetreader.screens.detail
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -9,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
@@ -23,8 +25,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalResources
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.core.text.HtmlCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import coil.compose.rememberAsyncImagePainter
@@ -101,10 +107,33 @@ fun BookDetail(book: Book, navController: NavController) {
     )
     Text(text = "Authors: ${volumeInfo.authors}")
     Text(text = "Page Count: ${volumeInfo.pageCount}")
-    Text(text = "Categories: ${volumeInfo.categories}", style = MaterialTheme.typography.titleSmall)
+    Text(
+        text = "Categories: ${volumeInfo.categories}",
+        style = MaterialTheme.typography.titleSmall,
+        overflow = TextOverflow.Ellipsis,
+        maxLines = 3
+    )
     Text(
         text = "Published: ${volumeInfo.publishedDate}",
         style = MaterialTheme.typography.titleSmall
     )
     Spacer(modifier = Modifier.height(5.dp))
+
+    val displayMetrics = LocalResources.current.displayMetrics
+    Surface(
+        modifier = Modifier
+            .height(displayMetrics.heightPixels.dp.times(0.09f))
+            .padding(4.dp),
+        shape = RectangleShape,
+        border = BorderStroke(1.dp, Color.DarkGray)
+    ) {
+        val cleanDescription =
+            HtmlCompat.fromHtml(volumeInfo.description, HtmlCompat.FROM_HTML_MODE_LEGACY)
+                .toString()
+        LazyColumn(modifier = Modifier.padding(3.dp)) {
+            item {
+                Text(text = cleanDescription)
+            }
+        }
+    }
 }

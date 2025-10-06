216e6a6e141d9f61a762bc92d2d2a277f69b368d
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-32G-1T.local>
2025-10-06 14:09:32 -0700

    b38: setting up Repository

--- app/src/main/java/com/nguyen/jetreader/repository/Repository.kt
+++ app/src/main/java/com/nguyen/jetreader/repository/Repository.kt
@@ -1,31 +1,30 @@
 package com.nguyen.jetreader.repository
 
-import com.nguyen.jetreader.data.MyResult
+import com.nguyen.jetreader.data.Resource
 import com.nguyen.jetreader.model.Book
 import com.nguyen.jetreader.network.BookService
 import javax.inject.Inject
 
 class Repository @Inject constructor(private val service: BookService) {
 
-    suspend fun getAllBooks(query: String): MyResult<List<Book>> {
+    suspend fun getAllBooks(query: String): Resource<List<Book>> {
         return try {
+            Resource.Loading(data = true)
             val data = service.getAllBooks(query).items
-            if (data.isNotEmpty()) MyResult.Success(data)
-            else MyResult.Error(Exception("No books found"))
+            if (data.isNotEmpty()) Resource.Loading(data = false)
+            Resource.Success(data = data)
         } catch (e: Exception) {
-            MyResult.Error(e)
+            Resource.Error(message = e.message.toString())
         }
     }
 
-    suspend fun getBookById(bookId: String): MyResult<Book> {
+    suspend fun getBookById(bookId: String): Resource<Book> {
         return try {
+            Resource.Loading(data = true)
             val data = service.getBookById(bookId)
-            if (data.toString().isNotEmpty())
-                MyResult.Success(data)
-            else
-                MyResult.Error(Exception("No books found"))
+            Resource.Success(data = data)
         } catch (e: Exception) {
-            MyResult.Error(e)
+            Resource.Error(e.message.toString())
         }
     }
 }
